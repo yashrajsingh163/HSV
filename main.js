334534a9f732991a6f0db30d93464ea1228b0f23
yashrajsingh163 <60474811+yashrajsingh163@users.noreply.github.com>
2020-08-06 13:53:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+	$("#div1").click(function(){
+
+		$("#div2").css('display','block');
+
+	});
+	$("#div2btn").click(function(){
+
+		$("#div3").css('display','block');
+
+	});
+	$("#div3btn").click(function(){
+
+		$("#div4").css('display','block');
+
+	});
+
+});
